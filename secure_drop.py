e20e42f5099ab86d2c771579d0ca6b21763c1ecd
Pratush <pratushkc@gmail.com>
2022-10-27 18:07:28 -0400

    Added if __name__ == "__main__" condition to prevent secure_drop.py from running if referenced by another module.

--- secure_drop.py
+++ secure_drop.py
@@ -1,30 +1,31 @@
 from registration import register_new_user
 
+def run():
+    # number of users (temporary placeholder for file scan)
+    userCount = 0
 
-# number of users (temporary placeholder for file scan)
-userCount = 0
-
-if userCount == 0:
-    print("No users are registered with this client.")
-    print("Do you want to register a new user (y/n)?")
-    # call registration function from registration.py
+    if userCount == 0:
+        print("No users are registered with this client.")
+        print("Do you want to register a new user (y/n)?")
+        # call registration function from registration.py
     
-    while True:
-        selection = input()
-        selection = selection.lower()
-        if selection == 'y' or selection == 'n':
-            break
+        while True:
+            selection = input()
+            selection = selection.lower()
+            if selection == 'y' or selection == 'n':
+                break
+            else:
+                print("Error: Invalid input.")
+                print("Do you want to register a new user (y/n)?")
+
+        if selection == 'y':
+            register_new_user()
         else:
-            print("Error: Invalid input.")
-            print("Do you want to register a new user (y/n)?")
-
-    if selection == 'y':
-        register_new_user()
+            print("Exiting Secure Drop")
     else:
-        print("Exiting Secure Drop")
-else:
-    # call login function from login.py
-    pass
+        # call login function from login.py
+        pass
 
 
-    
+if __name__ == "__main__":
+    run()
