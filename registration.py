4f4abd6ce485e669253f733fbe15ca383e6f7b4f
Pratush <pratushkc@gmail.com>
2022-10-26 23:41:20 -0400

    Added a simple menu for when booting the program for the first time (with no users registered).

--- registration.py
+++ registration.py
@@ -1,1 +1,5 @@
 # This folder will contain the user registration processes 
+
+def register_new_user():
+    print("Registering new user.")
+    pass
--- secure_drop.py
+++ secure_drop.py
@@ -1,10 +1,30 @@
-# This is the executable for the project 
+from registration import register_new_user
 
 
-# Options Menu
+# number of users (temporary placeholder for file scan)
+userCount = 0
 
-    # Registration
+if userCount == 0:
+    print("No users are registered with this client.")
+    print("Do you want to register a new user (y/n)?")
+    # call registration function from registration.py
+    
+    while True:
+        selection = input()
+        selection = selection.lower()
+        if selection == 'y' or selection == 'n':
+            break
+        else:
+            print("Error: Invalid input.")
+            print("Do you want to register a new user (y/n)?")
 
-    # Login 
+    if selection == 'y':
+        register_new_user()
+    else:
+        print("Exiting Secure Drop")
+else:
+    # call login function from login.py
+    pass
+
 
     
