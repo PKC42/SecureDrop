69c5d597ef26ca82183491a646ac886aa1777342
Pratush <pratushkc@gmail.com>
2022-10-29 12:18:12 -0400

    Added a utilities.py file for misc functions.

--- registration.py
+++ registration.py
@@ -1,5 +1,13 @@
 # This folder will contain the user registration processes 
 
 def register_new_user():
-    print("Registering new user.")
-    pass
+    print("Enter Full Name:")
+    name = input()
+    print("Enter email address:")
+    email = input()
+    print("Enter password")
+    password_1 = input()
+    print("Re-enter password")
+    password_2 = input()
+    
+    
